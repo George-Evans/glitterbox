489a74d2f4adf62981414ee2357d5f6146f8fec2
George Evans <george.r.evans@googlemail.com>
2018-06-15 13:55:09 +0200

    testing turning config.assets.compile back to false in production.rb now that local precompile works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,8 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.digest = true
+  config.assets.compile = false
 
   # Server Static Assets
   config.serve_static_assets = true
