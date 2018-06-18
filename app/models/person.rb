ec8d493d8995ffd059279f62e2574a4051ffa375
George Evans <george.r.evans@googlemail.com>
2018-06-18 18:28:21 +0200

    removed image_url validation from person model to try seeding

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,6 @@
 class Person < ApplicationRecord
 	validates :name, presence: true
 	validates :bio, presence: true
-	validates :image_url, presence: true
 
 	mount_uploader :image_url, PersonUploader
 end
